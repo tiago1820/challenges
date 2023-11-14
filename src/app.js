3bf8588235aa73e2c9cb75a7fd66e5ea31f6f3b5
Tiago de Oliveira <tiago.zdo@gmail.com>
2023-11-14 19:07:49 -0300

    A basic server was created with Express.

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = require('./routes/index');
+const server = express();
+
+// Middlewares
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+});
+
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+
+server.use('/my-erp', router);
+
+module.exports = server;
